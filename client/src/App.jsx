15dd7e351d242f396c47bbd5dd7962c504b53ee7
noyononline <noyononlinen@gmail.com>
2024-06-24 01:57:34 +0600

    ecommerce website frontend complate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home';
-import Shops from './pages/Shops';
-import Card from './pages/Card';
-import Details from './pages/Details';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Shipping from './pages/Shipping';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Shops from "./pages/Shops";
+import Card from "./pages/Card";
+import Details from "./pages/Details";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Shipping from "./pages/Shipping";
 
 function App() {
   return (
+    // route handing for routes
     <BrowserRouter>
-    <Routes>
-      <Route path='/register' element={<Register />}></Route>
-      <Route path='/login' element={<Login />}></Route>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/shops' element={<Shops />}></Route>
-      <Route path='/card' element={<Card />}></Route>
-      <Route path='/product/details/:slug' element={<Details />}></Route>
-      <Route path='/shipping' element={<Shipping /> }></Route>
-    </Routes>
+      <Routes>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/shops" element={<Shops />}></Route>
+        <Route path="/card" element={<Card />}></Route>
+        <Route path="/product/details/:slug" element={<Details />}></Route>
+        <Route path="/shipping" element={<Shipping />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
